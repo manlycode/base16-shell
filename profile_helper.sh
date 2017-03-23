3e6b3e419166322674337aff7b779c3a3d230af8
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2017-03-23 11:12:45 -0400

    @WIP

--- profile_helper.sh
+++ profile_helper.sh
@@ -18,16 +18,22 @@ _base16()
 {
   local script=$1
   local theme=$2
+  local vim_bg=$3
+  echo "vim_bg: $vim_bg"
   [ -f $script ] && . $script
   ln -fs $script ~/.base16_theme
   export BASE16_THEME=${theme}
-  echo -e "if \0041exists('g:colors_name') || g:colors_name != 'base16-$theme'\n  colorscheme base16-$theme\nendif" >| ~/.vimrc_background
+  echo -e "if \0041exists('g:colors_name') ||\n set bg=$vim_bg\n g:colors_name != 'base16-$theme'\n  colorscheme base16-$theme\nendif" >| ~/.vimrc_background
 }
 FUNC
 for script in $script_dir/scripts/base16*.sh; do
   script_name=${script##*/}
   script_name=${script_name%.sh}
+  vim_bg=$(echo $script_name | awk 'match($0, /(light|dark)/) {print substr($0, RSTART, RLENGTH)}')
+
+  echo "vim_bg: $vim_bg"
+
   theme=${script_name#*-}
   func_name="base16_${theme}"
-  echo "alias $func_name=\"_base16 \\\"$script\\\" $theme\""
+  echo "alias $func_name=\"_base16 \\\"$script\\\" $theme\\\" $vim_bg\""
 done;
